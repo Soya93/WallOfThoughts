30766a85e3b0940b8754ee6e1dbe201b9bc6ed6c
Sara <sayo.panda.sn@gmail.com>
2016-09-30 13:29:09 +0200

    Revert "The rest"
    This reverts commit 370afda1e2261a22b0bdd575632d783635904c87.

--- testDrawing/ViewController.swift
+++ testDrawing/ViewController.swift
@@ -9,74 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var drawView: DrawableView!
-    
-    let color: [String] = ["000000",
-                           "4CD964",
-                           "5AC8FA",
-                           "007AFF",
-                           "FF2D55",
-                           "FF3B30",
-                           "FF9500",
-                           "FFCC00"]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-}
-
-    @IBAction func colorPressed(sender: UIBarButtonItem) {
-        var id = 0
-        if sender.tag >= 0 || sender.tag <= 7 {
-            id = sender.tag
-        }
-        drawView.drawColor = self.hexStringToUIColor(color[id])
-        
     }
 
-    @IBAction func erase(sender: UIBarButtonItem) {
-        drawView.drawColor = UIColor.whiteColor()
-        drawView.drawWidth = 20.0
-    }
-   
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
- // Get the new view controller using segue.destinationViewController.
- // Pass the selected object to the new view controller.
- }
-*/
-    
-    func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
-        
-        if (cString.hasPrefix("#")) {
-            cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
-        }
-        
-        if ((cString.characters.count) != 6) {
-            return UIColor.grayColor()
-        }
-        
-        var rgbValue:UInt32 = 0
-        NSScanner(string: cString).scanHexInt(&rgbValue)
-        
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
- 
+
 }
 
